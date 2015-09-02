c6dbc4c29da7ff549c45d7d5b594e8c7dc6124a3
Neil Trini Lasrado <neil@frappe.io>
2015-09-02 17:13:11 +0530

    New App Created

---
+++ education/config/desktop.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from frappe import _
+
+def get_data():
+	return {
+		"Education": {
+			"color": "grey",
+			"icon": "octicon octicon-mortar-board",
+			"type": "module",
+			"label": _("Education")
+		}
+	}
