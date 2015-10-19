842d0ed894d8e8075d95b0f48355dc45e0243b8a
Neil Trini Lasrado <neil@frappe.io>
2015-10-19 16:13:40 +0530

    Added validation to prevent multiple students to be created against a single applicant

--- education/academics/doctype/student/student.py
+++ education/academics/doctype/student/student.py
@@ -9,7 +9,14 @@ from frappe.model.document import Document
 class Student(Document):
 	def validate(self):
 		self.title = self.first_name + " " + self.middle_name + " " +self.last_name
-		self.update_applicant_status()
+		if self.student_applicant:
+			self.check_unique()
+			self.update_applicant_status()
+		
+	def check_unique(self):
+		student = frappe.db.sql("select name from `tabStudent` where student_applicant=%s and name!=%s", (self.student_applicant, self.name))
+		if student:
+			frappe.throw("Student {0} exist against student applicant {1}".format(student[0][0], self.student_applicant))
 		
 	def update_applicant_status(self):
 		if self.student_applicant:
