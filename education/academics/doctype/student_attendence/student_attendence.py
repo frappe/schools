11ac1addccfb58bbe06c7395f94c8ac9c2d200e7
Neil Trini Lasrado <neil@frappe.io>
2015-11-09 18:40:30 +0530

    Added validations to Student Attendence

--- education/academics/doctype/student_attendence/student_attendence.py
+++ education/academics/doctype/student_attendence/student_attendence.py
@@ -5,6 +5,16 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from frappe import _
 
 class StudentAttendence(Document):
-	pass
+	def validate(self):
+		self.validate_duplication()
+		
+	def validate_duplication(self):
+		attendence_records= frappe.db.sql("""select name from `tabStudent Attendence` where \
+			student= %s and course_schedule= %s and name != %s""",
+			(self.student, self.course_schedule, self.name))
+		if attendence_records:
+			frappe.throw(_("Attendence Record {0} exists against Student {1} for Course Schedule {2}")
+				.format(attendence_records[0][0], self.student, self.course_schedule))
