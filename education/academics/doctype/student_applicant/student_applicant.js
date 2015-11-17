d5472cd2aa5cbc919c85220fe450c5cc61430dc8
Neil Trini Lasrado <neil@frappe.io>
2015-11-17 12:30:30 +0530

    Fixed create student button in Student Applicant

--- education/academics/doctype/student_applicant/student_applicant.js
+++ education/academics/doctype/student_applicant/student_applicant.js
@@ -1,16 +1,16 @@
 frappe.ui.form.on("Student Applicant", {
 	refresh: function(frm) {
 		if(frm.doc.application_status== "Approved" && frm.doc.docstatus== 1 ) {
-			frm.add_custom_button(__("Create Student"), frm.events.get_students, "btn-default");
+			frm.add_custom_button(__("Create Student"), function() {
+				frm.events.create_student(frm)
+			});
 		}
 	},
 	
-	
+	create_student: function(frm) {
+		frappe.model.open_mapped_doc({
+			method: "education.academics.doctype.student_applicant.student_applicant.make_student",
+			frm: frm
+		})
+	}
 });
-
-cur_frm.cscript.create_student = function() {
-	frappe.model.open_mapped_doc({
-		method: "education.academics.doctype.student_applicant.student_applicant.make_student",
-		frm: cur_frm
-	})
-}
