cf20a7542172e08b5aadca8b682e3758a40d5f70
Anand Doshi <anand@erpnext.com>
2016-04-07 16:37:46 +0530

    [fix] prepare for versioning

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-import os
+from pip.req import parse_requirements
 
 version = '0.0.1'
+requirements = parse_requirements("requirements.txt", session="")
 
 setup(
     name='schools',
@@ -13,5 +14,6 @@ setup(
     packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
-    install_requires=("frappe",),
+	install_requires=[str(ir.req) for ir in requirements],
+	dependency_links=[str(ir._link) for ir in requirements if ir._link]
 )
