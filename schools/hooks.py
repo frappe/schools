ce3e15e81feb9d9656ab5957f6ac7cbe58de1d25
Neil Trini Lasrado <neil@frappe.io>
2016-05-02 15:50:30 +0530

    Fixes in links

--- schools/hooks.py
+++ schools/hooks.py
@@ -22,7 +22,7 @@ required_apps = ["erpnext"]
 
 # include js, css files in header of desk.html
 # app_include_css = "/assets/schools/css/schools.css"
-# app_include_js = "/assets/schools/js/schools.js"
+app_include_js = "/assets/js/schools.min.js"
 
 # include js, css files in header of web template
 # web_include_css = "/assets/schools/css/schools.css"
--- schools/public/js/conf.js
+++ schools/public/js/conf.js
@@ -6,13 +6,6 @@ frappe.provide('schools');
 // add toolbar icon
 $(document).bind('toolbar_setup', function() {
 	frappe.app.name = "Schools";
-
-	frappe.help_feedback_link = '<p><a class="text-muted" \
-		href="https://discuss.erpnext.com">Feedback</a></p>'
-
-
-	$('.navbar-home').html('<img class="erpnext-icon" src="'+
-			frappe.urllib.get_base_url()+'/assets/erpnext/images/erp-icon.svg" />');
-
 	$('[data-link="docs"]').attr("href", "http://frappe.github.io/schools/")
+	$('[data-link="issues"]').attr("href", "https://github.com/frappe/schools/issues")
 });
