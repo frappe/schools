6c351436ae8abbef4c73d5c97c5df203c2103582
Nabin Hait <nabinhait@gmail.com>
2016-05-20 12:50:44 +0600

    bumped to version 0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 from pip.req import parse_requirements
 
-version = '0.2.0'
+version = '0.3.0'
 requirements = parse_requirements("requirements.txt", session="")
 
 setup(
