106e9823a0dd4c639ddcc7afca769d9ccee5cd55
Neil Trini Lasrado <neil@frappe.io>
2016-05-31 09:56:40 +0530

    UI changes for Version 7

--- schools/academics/doctype/student/student.js
+++ schools/academics/doctype/student/student.js
@@ -1,38 +1,7 @@
 frappe.ui.form.on("Student", "refresh", function(frm) {
 	if(!cur_frm.doc.__islocal) {
-		frm.add_custom_button(__("Programs Enrolled"), function() {
-			frappe.route_options = {
-				student: frm.doc.name
-			}
-			frappe.set_route("List", "Program Enrollment");
-		});
-		
-		frm.add_custom_button(__("Student Groups"), function() {
-			frappe.route_options = {
-				"Student Group Student.student": frm.doc.name
-			}
-			frappe.set_route("List", "Student Group");
-		});
-		
-		frm.add_custom_button(__("Fees"), function() {
-			frappe.route_options = {
-				student: frm.doc.name
-			}
-			frappe.set_route("List", "Fees");
-		});
-		
-		frm.add_custom_button(__("Attendance"), function() {
-			frappe.route_options = {
-				student: frm.doc.name
-			}
-			frappe.set_route("List", "Student Attendance");
-		});	
-		
-		frm.add_custom_button(__("Examination"), function() {
-			frappe.route_options = {
-				"Examination Result.student": frm.doc.name
-			}
-			frappe.set_route("List", "Examination");
-		});	
+		frm.dashboard.heatmap_message = __('This is based on the attendance of this Student');
+		frm.dashboard.show_heatmap = true;
+		frm.dashboard.show_dashboard();
 	}
 });
---
+++ schools/academics/doctype/student/student_links.py
@@ -0,0 +1,13 @@
+from frappe import _
+
+links = {
+	'fieldname': 'student',
+	'transactions': [
+		{
+			'items': ['Student Group', 'Student Attendance', 'Program Enrollment' ]
+		},
+		{
+				'items': ['Fees', 'Examination']
+		}
+	]
+}
