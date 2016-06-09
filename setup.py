cb2daa4b1d5d1ddc0c882337bc2578e82a95c8a1
Anand Doshi <anand@erpnext.com>
2016-06-09 16:34:06 +0530

    [fix] change in versioning: store __version__ in __init__.py

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 from pip.req import parse_requirements
+import re, ast
 
-version = '0.3.0'
+# get version from __version__ variable in schools/__init__.py
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('schools/__init__.py', 'rb') as f:
+    version = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
+
 requirements = parse_requirements("requirements.txt", session="")
 
 setup(
