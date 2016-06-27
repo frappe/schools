e243083f3b64767841436a81989629d603a67012
Neil Trini Lasrado <neil@frappe.io>
2016-06-27 16:08:27 +0530

    code cleanup

--- schools/templates/pages/courses.py
+++ schools/templates/pages/courses.py
@@ -3,19 +3,12 @@
 
 from __future__ import unicode_literals
 import frappe
-import json
 
 def get_context(context):
-		
 	context.no_cache = 1
 	context.show_sidebar = True
 	course = frappe.get_doc('Course', frappe.form_dict.course)
 
 	course.has_permission('read')
-	
 	context.doc = course
-
-	
-	context.discuss = frappe.db.sql('''select * from tabDiscussion as discuss 
-							where discuss.course = %s
-						''',(course.name), as_dict = True)
+	context.discuss = frappe.db.sql('''select * from tabDiscussion as discuss where discuss.course = %s''',(course.name), as_dict = True)
