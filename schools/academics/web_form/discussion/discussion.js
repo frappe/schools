94259ac13da6fe5cc21f55f49053d71333fb98ca
Neil Trini Lasrado <neil@frappe.io>
2016-06-29 16:03:42 +0530

    Hacked discussion form for users with read permission

--- schools/academics/web_form/discussion/discussion.js
+++ schools/academics/web_form/discussion/discussion.js
@@ -1,3 +1,13 @@
 frappe.ready(function() {
+	var form = $('form[data-web-form="discussion"]'),
+		owner = form.attr('data-owner');
+
+	var is_new = window.location.href.search("new=1")
+	if(owner != frappe.session.user && is_new<0) {
+		form.toggle(false);
+		$('div[class="page-header-actions-block"]').toggle(false);
+		
+		$('<p>' + $('[name="description"]').val() + '</p>').insertBefore('.comments');
+	}
 	// bind events here
 })
