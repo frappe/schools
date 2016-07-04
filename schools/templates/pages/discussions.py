580c481a4e2a902c6057ec352fb4c22892ce860e
shubham7saxena <ug201310033@iitj.ac.in>
2016-07-04 12:45:23 +0530

    comments are working now

--- schools/templates/pages/discussions.py
+++ schools/templates/pages/discussions.py
@@ -3,17 +3,19 @@
 
 from __future__ import unicode_literals
 import frappe
+from frappe.website.utils import get_comment_list
 
 def get_context(context):
-	discussion = frappe.get_doc('Discussion', frappe.form_dict.discussion)
-	portal_items = [{'reference_doctype': u'Topic', 'route': u"/topic?course=" + str(discussion.course), 'show_always': 0L, 'title': u'Topics'},
-				{'reference_doctype': u'Discussion', 'route': u"/discussion?course=" + str(discussion.course), 'show_always': 0L, 'title': u'Discussions'},
+	context.doc = frappe.get_doc('Discussion', frappe.form_dict.discussion)
+	portal_items = [{'reference_doctype': u'Topic', 'route': u"/topic?course=" + str(context.doc.course), 'show_always': 0L, 'title': u'Topics'},
+				{'reference_doctype': u'Discussion', 'route': u"/discussion?course=" + str(context.doc.course), 'show_always': 0L, 'title': u'Discussions'},
 
 	]
-	context.no_cache = 0
 	context.show_sidebar = True
-	discussion.has_permission('read')
 	context.sidebar_items = portal_items
-
-	context.sidebar_title = discussion.course
-	context.doc = discussion
+	context.no_cache = 0
+	context.doc.has_permission('read')
+	context.sidebar_title = context.doc.course
+	context.reference_doctype = "Discussion"
+	context.reference_name = context.doc.name
+	context.comment_list = get_comment_list(context.doc.doctype,context.doc.name)
